02cac2b75afcfb0f007fe1d04a3af74c5ab3c664
gi-ong <pugo9165@gmail.com>
2023-04-29 18:09:33 +0900

    Context API + Reducer Pattern

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import App5 from './App5';
 import App6 from './App6';
 import App7 from './App7';
 import App8 from './App8';
+import App9 from './App9';
 import TodoList from './TodoList';
 import EpisodeList from 'EpisodeList';
 import reportWebVitals from './reportWebVitals';
@@ -16,7 +17,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App8 />
+    <App9 />
   </React.StrictMode>
 );
 
